1666cfd1c48007b7dbc578768bd127ca5d0dd1df
Lalitha <lalitha.d339@gmail.com>
2023-08-10 05:21:46 +0530

    terraform variables

---
+++ main.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "instance" {
+
+  ami           = data.aws_ami.ami.id
+  instance_type = var.instance_type
+  vpc_security_group_ids =var.security_group
+  tags = {
+    Name = var.name
+  }
+
+
+}
+
+resource "aws_route53_record" "record" {
+  zone_id = var.zone_id
+  name    = "${var.name}-dev.dljrobo.online"
+  type    = "A"
+  ttl     = 30
+  records = [ aws_instance.instance.private_ip ]
+}
+
+resource "null_resource" "ansible"{
+    depends_on = [
+    aws_route53_record.record
+    ]
+
+    provisioner "local-exec" {
+      command = <<EOF
+      sudo su -
+      cd /root/roboshop-ansible_auto
+      git pull
+      sleep 30
+      ansible-playbook -i ${var.name}-dev.dljrobo.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
+
+      EOF
+    }
+}
